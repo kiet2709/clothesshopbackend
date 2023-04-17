4f3d360c0a94668f5034d62c6db93213ffcbfa25
unknown <tuankietlenguyen2709@gmail.com>
2023-04-18 01:45:50 +0700

    create migration clothes

---
+++ database/migrations/2023_04_17_184310_create_clothes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClothesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clothes', function (Blueprint $table) {
+            $table->id();
+            $table->string('description');
+            $table->string('name');
+            $table->integer('price');
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('image_id')->references('id')->on('images')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clothes');
+    }
+}
