f7bb50db8412388ad2a7a905b8303c25341b659f
unknown <tuankietlenguyen2709@gmail.com>
2023-04-18 02:06:31 +0700

    create migration orders

---
+++ database/migrations/2023_04_17_190402_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('deliver_cost');
+            $table->string('deliver_method');
+            $table->date('order_date');
+            $table->foreign('order_track_id')->references('id')->on('order_track')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
